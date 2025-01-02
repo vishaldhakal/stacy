ffeedfa2ce9bf0ace1f2009edaabf724f0341589
vishaldhakal <vishaldhakal96@gmail.com>
2025-01-03 02:00:17 +0545

    updated design

--- components/resale/ResaleCard.jsx
+++ components/resale/ResaleCard.jsx
@@ -104,7 +104,7 @@ const ResaleCard = ({ curElem, small = false, showDecreasedPrice = false }) => {
   };
 
   return (
-    <section className="relative transition-all duration-200 transform bg-white group rounded-2xl p-0 hover:shadow-lg hover:rounded-t-2xl  hover:-translate-y-1 overflow-hidden">
+    <section className="relative transition-all duration-200 transform bg-white group rounded-xl hover:shadow-lg hover:-translate-y-1 overflow-hidden border border-gray-200">
       <Link
         href={generateURL({
           cityVal: curElem.CountyOrParish,
@@ -114,113 +114,86 @@ const ResaleCard = ({ curElem, small = false, showDecreasedPrice = false }) => {
       >
         <div className="lg:px-0 h-full w-full">
           <div className={`flex flex-col overflow-hidden relative`}>
-            <div className={`${"h-52 sm:h-80"} overflow-hidden relative`}>
-              <div
-                className={`${
-                  small ? "h-44" : "h-52 sm:h-80"
-                } sm:h-80 relative z-10 rounded-t-2xl`}
-              >
+            <div className="h-[220px] overflow-hidden relative">
+              <div className="h-full relative z-10">
                 {loadingImage ? (
                   <Spinner />
                 ) : (
                   <img
-                    className="object-cover w-full h-full transition-all duration-200 transform group-hover:scale-110 rounded-t-2xl"
+                    className="object-cover w-full h-full transition-all duration-200 transform group-hover:scale-110"
                     src={imgUrl}
                     width="900"
                     height="800"
                     alt="property image"
-                    onError={(e) => {
-                      console.log("Trigerring error");
-                      handleImageError(e);
-                    }}
+                    onError={handleImageError}
                   />
                 )}
-
-                {/* <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent opacity-50"></div> */}
               </div>
 
               <div className="absolute bottom-3 left-2 flex flex-row z-20">
-                <div className="text-black text-[0.7rem] p-[3px] px-2 shadow-2xl rounded-md mx-1 bg-white flex items-center">
-                  {curElem.PropertySubType}{" "}
+                <div className="text-black text-xs p-1 px-2 shadow-sm rounded-md mx-1 bg-white/90 flex items-center">
+                  {curElem.PropertySubType}
                 </div>
                 {curElem.ApproxSquareFootage && (
-                  <div className="text-black text-[0.7rem] p-[3px] px-2 shadow-2xl rounded-md mx-1 bg-white items-center hidden sm:block">
-                    <img
-                      src="/resale-card-img/ruler.svg"
-                      className="w-3 mr-[2px] inline"
-                      alt="washrooms"
-                    />
-                    <span>{curElem.ApproxSquareFootage} Sq.Ft.</span>
+                  <div className="text-black text-xs p-1 px-2 shadow-sm rounded-md mx-1 bg-white/90 items-center">
+                    {curElem.ApproxSquareFootage} Sq.Ft.
                   </div>
                 )}
-                {/* <div className="text-black text-[0.7rem] p-[3px] px-2 shadow-2xl rounded-md mx-1 bg-white flex items-center">
-                </div> */}
               </div>
             </div>
-            <div className="flex-1 sm:px-3 pt-2 pb-4 px-2">
+            <div className="flex-1 p-4">
               <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center">
-                <h2 className="font-bold text-2xl sm:text-2xl items-center justify-start mt-2 sm:my-2">
+                <h2 className="font-bold text-2xl items-center justify-start">
                   <span className="font-bold text-black">{price}</span>
                   {curElem.SaleLease === saleLease.lease.value && (
                     <span> /mo</span>
                   )}
                 </h2>
-                <div className="text-xs font-medium text-[#CC0B0B] mb-1 sm:mb-0">
+                <div className="text-xs font-medium text-red-600">
                   <TimeAgo
                     modificationTimestamp={curElem.OriginalEntryTimestamp}
                   />
                 </div>
               </div>
-              {/* <p className="mb-0 fs-mine text-limit font-md pb-0">
-                  {" "}
-                  MLS® #{curElem.ListingKey}
-                </p> */}
-              <span className={`text-black text-xs`}>
-                <div className="flex flex-row justify-start">
-                  {curElem.BedroomsTotal && (
-                    <div className="flex items-center mr-3">
-                      <img
-                        src="/resale-card-img/bedrooms.svg"
-                        className="w-3 mr-[2px] inline"
-                        alt="bedrooms"
-                      />
-                      <span>
-                        {Math.floor(curElem.BedroomsTotal)}{" "}
-                        <span className="hidden sm:inline">Bed</span>
-                      </span>
-                    </div>
-                  )}
-                  {curElem.BathroomsTotalInteger && (
-                    <div className="flex items-center mr-3">
-                      <img
-                        src="/resale-card-img/bathrooms.svg"
-                        className="w-3 mr-[2px] inline"
-                        alt="washrooms"
-                      />
-                      <span>
-                        {Math.floor(curElem.BathroomsTotalInteger)}{" "}
-                        <span className="hidden sm:inline">Bath</span>
-                      </span>
-                    </div>
-                  )}
-                  {curElem.GarageParkingSpaces && (
-                    <div className="flex items-center mr-3">
-                      <img
-                        src="/resale-card-img/garage.svg"
-                        className="w-3 mr-[2px] inline"
-                        alt="washrooms"
-                      />
-                      <span>
-                        {Math.floor(curElem.GarageParkingSpaces)}{" "}
-                        <span className="hidden sm:inline">Garage</span>
-                      </span>
-                    </div>
-                  )}
-                </div>
-              </span>
-              <div className="flex flex-row justify-between my-1">
+              <div className="flex items-center gap-4 mt-3 text-sm">
+                {curElem.BedroomsTotal && (
+                  <div className="flex items-center">
+                    <img
+                      src="/resale-card-img/bedrooms.svg"
+                      className="w-3 mr-1"
+                      alt="bedrooms"
+                    />
+                    <span>{Math.floor(curElem.BedroomsTotal)} bed</span>
+                  </div>
+                )}
+                {curElem.BathroomsTotalInteger && (
+                  <div className="flex items-center">
+                    <img
+                      src="/resale-card-img/bathrooms.svg"
+                      className="w-3 mr-1"
+                      alt="washrooms"
+                    />
+                    <span>
+                      {Math.floor(curElem.BathroomsTotalInteger)} bath
+                    </span>
+                  </div>
+                )}
+                {curElem.GarageParkingSpaces && (
+                  <div className="flex items-center">
+                    <img
+                      src="/resale-card-img/garage.svg"
+                      className="w-3 mr-1"
+                      alt="garage"
+                    />
+                    <span>
+                      {Math.floor(curElem.GarageParkingSpaces)} garage
+                    </span>
+                  </div>
+                )}
+              </div>
+              <div className="mt-3">
                 <div className="text-black">
-                  <div className="text-dark text-sm">
+                  <div className="text-sm">
                     {curElem.StreetName ? (
                       `${curElem.StreetNumber} ${curElem.StreetName}${" "}
                     ${curElem.StreetSuffix} ${curElem.CountyOrParish}, Ontario`
@@ -230,21 +203,14 @@ const ResaleCard = ({ curElem, small = false, showDecreasedPrice = false }) => {
                   </div>
                 </div>
               </div>
-              <div className="text-xs text-gray-600">
+              <div className="text-xs text-gray-500 mt-2">
                 MLS® {curElem.ListingKey}
-              </div>
-              <div className="text-xs text-gray-600">
-                Listed by {curElem.ListOfficeName}
               </div>
             </div>
           </div>
         </div>
       </Link>
-      <div
-        className={`absolute ${
-          small ? "top-[8rem] sm:top-[10rem]" : "sm:top-[18.5rem] top-[9rem]"
-        } right-2 z-10`}
-      >
+      <div className={`absolute ${"top-4"} right-2 z-10`}>
         <Favorite
           isFavorite={isFavorite}
           toggleFavorite={toggleFavorite}
